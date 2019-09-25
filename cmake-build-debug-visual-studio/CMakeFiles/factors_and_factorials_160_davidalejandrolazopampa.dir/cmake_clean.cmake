387a90f3a8a21d2a781efb7c1e4fa8b4b937986c
davidalejandrolazopampa <40555746+davidalejandrolazopampa@users.noreply.github.com>
2019-09-25 15:24:23 -0500

    Acepto

---
+++ cmake-build-debug-visual-studio/CMakeFiles/factors_and_factorials_160_davidalejandrolazopampa.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/factors_and_factorials_160_davidalejandrolazopampa.dir/main.cpp.obj"
+  "factors_and_factorials_160_davidalejandrolazopampa.pdb"
+  "factors_and_factorials_160_davidalejandrolazopampa.exe"
+  "factors_and_factorials_160_davidalejandrolazopampa.exe.manifest"
+  "factors_and_factorials_160_davidalejandrolazopampa.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/factors_and_factorials_160_davidalejandrolazopampa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,55 @@
+//
+// Created by David Lazo on 19/09/2019.
+//
+#include <iostream>
+#include <map>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+map<int,int> factoresPrimos;
+int num;
+
+void primeFactors(int n){
+    while (n % 2 == 0)
+    {
+        factoresPrimos[2]++;
+        n = n/2;
+    }
+
+
+    for (int i = 3; i <= sqrt(n); i = i + 2)
+    {
+        while (n % i == 0)
+        {
+            factoresPrimos[i]++;
+            n = n/i;
+        }
+    }
+
+    if (n > 2)
+        factoresPrimos[n]++;
+}
+
+int main() {
+
+    while(cin>>num, num!=0){
+
+        cout<<setw(3)<<num<<"! =";
+        while(num != 1){
+            primeFactors(num);
+            num--;
+        }
+        int contador = 0;
+        for(auto it= factoresPrimos.begin();it != factoresPrimos.end()--; it++){
+            if(contador == 15) {
+                cout<<endl<<"      ";
+                contador = 0;
+            }
+            cout<<setw(3)<<it->second;
+            contador++;
+        }
+        cout<<endl;
+        factoresPrimos.clear();
+    }
+    return 0;
+}
